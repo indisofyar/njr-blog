1a1d19e6c463f57a361fc1985749b158cb2095c7
indisofyar <38954340+indisofyar@users.noreply.github.com>
2024-12-02 09:02:42 +0000

    Added database

--- mysite/settings/production.py
+++ mysite/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
@@ -6,3 +7,9 @@ try:
     from .local import *
 except ImportError:
     pass
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=1800),
+}
